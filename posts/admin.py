867e09dddb279da54259b6c8c556467d5a516824
Roy <tomas.cse.ju@gmail.com>
2018-05-01 18:53:30 +0900

    change

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Posts
+
+admin.site.register(Posts)
+class Meta:
+    verbose_name_plural = "Posts"
--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'posts/index.html')
+    return render(request,'posts/index.html',{
+        'title':'Latest Posts'
+    })
 
